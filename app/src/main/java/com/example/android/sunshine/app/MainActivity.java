357eea1424f392ec0c5d9fb46be3e26789dab2ad
Michael Dijamco <mdd233@cornell.edu>
2016-07-10 21:33:22 -0400

    Start lesson 04

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Order", "onPause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Order", "onResume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Order", "onStop");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Order", "onStart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Order", "onDestroy");
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d("Order", "onCreate");
+
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -39,25 +77,27 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
+        } else if (id == R.id.action_open_map){
+            String prefLocation =
+                    PreferenceManager
+                            .getDefaultSharedPreferences(this)
+                            .getString(
+                                    getString(R.string.pref_location_key)
+                                    ,getString(R.string.pref_location_default)
+                            );
+            Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                    .appendQueryParameter("q", prefLocation).build();
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+            mapIntent.setData(geoLocation);
+            if (mapIntent.resolveActivity(getPackageManager()) != null){
+                startActivity(mapIntent);
+            }
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 }
